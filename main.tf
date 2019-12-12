ad6e63e42ecab0375ca584cfbf96d1d70f93ac90
Robert Jordan <rjordan@deba.cl>
2019-12-12 12:16:51 -0800

    change cidr

--- main.tf
+++ main.tf
@@ -17,6 +17,5 @@ module "k8s_cluster" {
   name = "test-cluster"
   private_subnets = ["10.10.2.0/24"]
   public_subnets = ["10.10.1.0/24"]
-  vpc_cidr = "10.10.0.0/16"
+  vpc_cidr = "10.11.0.0/16"
 }
-//No Comment
