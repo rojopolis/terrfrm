50697e91e866d4f023af96ad063a6734c6e3e0e9
Robert Jordan <39421615+rojopolis@users.noreply.github.com>
2020-02-12 11:58:07 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,15 +8,6 @@ terraform {
   }
   required_version = "0.12.20"
 }
-
-//--------------------------------------------------------------------
-// Modules
-module "k8s_cluster" {
-  source  = "app.terraform.io/rojopolis/k8s-cluster/aws"
-  version = "0.1.0"
-  kubernetes_version = "1.14"
-  name = "test-cluster"
-  private_subnets = ["10.10.2.0/24"]
-  public_subnets = ["10.10.1.0/24"]
-  vpc_cidr = "10.11.0.0/16"
-}
+output "greeting" {
+  value = "Hello world"
+  }
